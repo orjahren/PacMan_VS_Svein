8279e9b2c69c706cdb71f39565c88898a1ee4938
Cobra Bubbles <mjoeng@hotmail.no>
2021-02-23 11:33:17 +0100

    La på svart bakgrund på pane root. Annet enn det er det ikke gjort noen endringer

--- src/main/java/com/mycompany/pacman/App.java
+++ src/main/java/com/mycompany/pacman/App.java
@@ -3,6 +3,7 @@ package com.mycompany.pacman;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,12 +15,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        Pane root = new Pane(); 
+        root.setStyle("-fx-background-color: black;");
+        Scene scene = new Scene(root); 
         stage.setScene(scene);
+        stage.setTitle("PacMan");
         stage.show();
     }
 
